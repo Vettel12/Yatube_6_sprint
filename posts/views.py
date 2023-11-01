9d48f140858b5c21fc948d88016b069aabfde87f
Valery <redbullyagodkin@gmail.com>
2023-11-01 20:29:01 +0300

    ok

--- posts/views.py
+++ posts/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from django.http import Http404
 
 from .models import Post
+from .models import Group
 
 def index(request):
     latest = Post.objects.order_by("-pub_date")[:11]
     return render(request, "index.html", {"posts": latest})
+
+def group_posts(request, slug):
+    try:
+        group = get_object_or_404(Group, slug=slug)
+        posts = Post.objects.filter(group=group).order_by("-pub_date")[:12]
+    except Http404:
+        group = None
+        posts = []
+
+    return render(request, "group.html", {"group": group, "posts": posts})
